d45357eed71cf877325f06b5df00d199fedecf98
Samwel <same030407@gmail.com>
2026-02-28 16:00:02 +0000

    added the player jumping and some walls for basic interaction

--- MainPanel.java
+++ MainPanel.java
@@ -6,7 +6,7 @@ import javax.swing.*;
 public class MainPanel extends JPanel implements KeyListener {
 
 
-    private player p1;
+    private Player p1;
     private final Timer timer;
 
 
@@ -15,34 +15,51 @@ public class MainPanel extends JPanel implements KeyListener {
         addKeyListener(this);
         setFocusable(true);
 
-        this.p1 = new player(100, 100, 10, 10);
+        this.p1 = new Player(100, 100, 10, 10);
         
         // setup timer
-        timer = new Timer(1, e -> {
+        timer = new Timer(15, e -> {
+            update();
             repaint();
         });
 
         timer.start();
     }
 
-
+    //draws all the items in the game
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
-
         p1.drawPlayer(g2);
+
+
+
+        //draws the ground and walls of the screen
+        g2.setColor(Color.GRAY);
+        g2.fillRect(0,610, 1500, 100);
+        g2.fillRect(0,0, 20, 1000);
+        g2.fillRect(1310, 0, 100, 1000);
     }
 
+    
     @Override
     public void keyReleased(KeyEvent e){/* not used */ }
 
     @Override
     public void keyTyped(KeyEvent e){ /* not used*/ }
 
+
+    //checks if the player has pressed any keys
     @Override
     public void keyPressed(KeyEvent e){
         p1.keyPressed(e);
+    }   
+
+    //checks for collision between objects and jumping 
+    public void update(){
+        p1.jump();
+        p1.collisionWithGroundAndWalls();
     }
 
 
--- player.java
+++ player.java
@@ -3,20 +3,24 @@ import java.awt.event.*;
 
 
 
-public class player implements KeyListener{
+public class Player implements KeyListener{
 
     public int x;
     public int y; 
     public int width;
     public int height;
-    
+    public boolean jumping;
+    public int jumpVel;
 
+    final int JUMP_VEL = 15;
 
-    public player(int x, int y, int width, int height){
+    public Player(int x, int y, int width, int height){
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
+        this.jumping = false;
+        this.jumpVel = JUMP_VEL;
         
     }
 
@@ -28,34 +32,56 @@ public class player implements KeyListener{
     @Override
     public void keyPressed(KeyEvent e){
 
-        switch(e.getKeyCode()){
-
-            case KeyEvent.VK_W:
-                this.y -= 5;
-                break;
-
-            case KeyEvent.VK_D:
-                this.x += 5;
-                break;
-
-            case KeyEvent.VK_A:
-                this.x -= 5;
-                break;
-
-            case KeyEvent.VK_S:
-                this.y += 5;
-                break;
+        if(e.getKeyCode() == KeyEvent.VK_W){
+            this.jumping = true;
+            this.jump();
         }
+
+        if(e.getKeyCode() == KeyEvent.VK_D) this.x += 5;
+
+        if(e.getKeyCode() == KeyEvent.VK_A) this.x -= 5;
     }
 
 
     public void jump(){
+        if(this.jumping){
+            System.out.println("jumping should be happening");
+            this.y -=  this.jumpVel;
+            this.jumpVel -= 1;
+        }
+
+        else this.y += 5; 
+
         
+    }
+
+    public void collisionWithGroundAndWalls(){
+
+        //checks collision with ground
+        if(this.checkCollision(0, 600, 1500, 1000)){
+            this.jumping = false;
+            this.y = 600;
+            this.jumpVel = JUMP_VEL;
+        }
+
+        //checks collision with the left wall
+        if(this.checkCollision(0, 0, 25, 1000)) this.x = 20;
+
+        if(this.checkCollision(1300, 0, 25, 1000)) this.x = 1300;
 
 
 
     }
 
+    public boolean checkCollision(
+        int ObjectX, int ObjectY, int ObjectWidth, int ObjectHeight){
+        
+        return (this.x >= ObjectX &&
+                this.x +this.width <= ObjectX + ObjectWidth) &&
+               (this.y >= ObjectY && 
+                this.y +this.height <= ObjectY + ObjectHeight);
+                     
+    }
 
     public void drawPlayer(Graphics2D g){
         g.setColor(Color.BLACK);
