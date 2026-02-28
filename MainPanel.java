e81eec5c99eca45de765d2e67fd381751926c91f
matt <m.greenhouse@hotmail.com>
2026-02-28 21:17:28 +0000

    we now have response to the keyboard AMAZING

--- MainPanel.java
+++ MainPanel.java
@@ -1,6 +1,7 @@
 
 import java.awt.*;
 import java.awt.event.*;
+import java.util.Arrays;
 import javax.swing.*;
 
 public class MainPanel extends JPanel implements KeyListener {
@@ -88,18 +89,22 @@ public class MainPanel extends JPanel implements KeyListener {
 
     public void addCharPass(char c) {
         if (!(c == '#')) {
-            String currString = password.getText();
-            String newString = currString + String.valueOf(c);
-            password.setText(newString);
+            char[] currPassword = password.getPassword();
+            char[] newPassword = Arrays.copyOf(currPassword, currPassword.length + 1);
+            newPassword[newPassword.length - 1] = c;
+
+            password.setText(new String(newPassword));
+            Arrays.fill(currPassword, '\0');
         }
     }
 
     public void removeCharPass() {
-        String currString = password.getText();
-        int strLength = currString.length();
-        if (!(strLength == 0)) {
-          String newString = currString.substring(0, strLength-1);
-          password.setText(newString);
+        char[] currPassword = password.getPassword();
+        int passLength = currPassword.length;
+        if (!(passLength == 0)) {
+          char[] newPassword = Arrays.copyOf(currPassword, passLength- 1);
+          password.setText(new String(newPassword));
+          Arrays.fill(currPassword, '\0');
         }
     }
 
@@ -143,8 +148,10 @@ public class MainPanel extends JPanel implements KeyListener {
     }
 
 
-    public void update(){
+    public void update() {
         p1.movement();
         p1.collisionWithGroundAndWalls();
+
+        this.addCharUsername(keyboard.checkCollisions(p1.x, p1.y, p1.width, p1.height));
     }
 }
