2900716972a0b4ab5d3d032ac5d26602a05a4f6d
ramanseuruk <raman.seuruk@gmail.com>
2026-03-01 10:53:25 +0000

    finally fucking done

--- Keyboard.java
+++ Keyboard.java
@@ -14,7 +14,7 @@ public class Keyboard {
 
     //adds some InteractionKeys 
     public InteractionKeys[] InteractionButtons = new InteractionKeys[2];
-    
+
 
     public void addChars() {
 
@@ -55,7 +55,7 @@ public class Keyboard {
 
     //draws keyboard and the boxes around stuff
     public void draw(Graphics2D g){
-        
+
         //draws the numbers
         for(NumberChar num : numbers) num.drawChar(g);
 
